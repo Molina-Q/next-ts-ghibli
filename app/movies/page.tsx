6097e48e2fadf04ec20b684dee9f9620d55ffb30
Molina-Q <quentinmolina1@gmail.com>
2024-06-10 21:47:33 +0200

    feat: Add movie creation form and API route

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,9 +1,212 @@
-import React from 'react'
+"use client"
 
-type Props = {}
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
 
-export default function Movies({}: Props) {
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { db } from "@/lib/db"
+import axios from "axios"
+import { useRouter } from "next/navigation"
+
+import { addDays, format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { Calendar } from "@/components/ui/calendar"
+import { cn } from "@/lib/utils"
+
+const formSchema = z.object({
+  title: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  duration: z.number().min(0, {
+    message: "Duration must be a positive number.",
+  }),
+  synopsis: z.string().min(10, {
+    message: "Synopsis must be at least 10 characters.",
+  }),
+  note: z.number().int().min(0).max(10, {
+    message: "Note must be an integer between 0 and 10.",
+  }),
+  releaseDate: z.date(),
+  directorId: z.number().int().positive({
+    message: "Director ID must be a positive integer.",
+  }),
+})
+
+export default function Movie() {
+  const director = 2;
+
+  const router = useRouter();
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      duration: 0,
+      synopsis: "",
+      note: 0,
+      releaseDate: new Date(),
+      directorId: 0,
+    },
+  })
+
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.post(`/api/movie/create`, { title: values.title, duration: values.duration, releaseDate: values.releaseDate, directorId: director });
+      // toast.success("Topic added !");
+      form.reset();
+
+      router.refresh();
+    } catch {
+      // toast.error("Something went wrong");
+      console.log("error")
+    }
+    console.log(values)
+  }
+
   return (
-    <div>Movies</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name of the movie</FormLabel>
+              <FormControl>
+                <Input placeholder="Howl and the moving castle" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="duration"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Duration</FormLabel>
+              <FormControl>
+                <Input type="number" placeholder="120" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is the duration of the movie in minutes.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="synopsis"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Synopsis</FormLabel>
+              <FormControl>
+                <Input placeholder="A movie about..." {...field} />
+              </FormControl>
+              <FormDescription>
+                This is a short description of the movie.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="note"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Note</FormLabel>
+              <FormControl>
+                <Input type="number" placeholder="7" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is the note of the movie between 0 and 10.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="releaseDate"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>Date of birth</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[240px] pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={(date) =>
+                      date > new Date() || date < new Date("1900-01-01")
+                    }
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormDescription>
+                Your date of birth is used to calculate your age.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+
+        <FormField
+          control={form.control}
+          name="directorId"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Director</FormLabel>
+              <FormControl>
+                <Input type="number" placeholder="7" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   )
 }
