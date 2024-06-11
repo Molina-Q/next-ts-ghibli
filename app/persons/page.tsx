517e263da6d15d28886f71ef5a634d8714b95a02
Molina-Q <quentinmolina1@gmail.com>
2024-06-11 21:00:20 +0200

    refactor: Update form validation and data transformation in movie creation

---
+++ app/persons/page.tsx
@@ -0,0 +1,168 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { db } from "@/lib/db"
+import axios from "axios"
+import { useRouter } from "next/navigation"
+
+import { addDays, format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { Calendar } from "@/components/ui/calendar"
+import { cn } from "@/lib/utils"
+
+const formSchema = z.object({
+    lastName: z.string().min(1, {
+        message: "name must not be empty.",
+    }),
+    firstName: z.string().min(1, {
+        message: "name must not be empty.",
+    }),
+    sex: z.string(),
+    birthdate: z.date(),
+})
+
+export default function MovieForm() {
+
+    const router = useRouter();
+
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            lastName: "",
+            firstName: "",
+            sex: "",
+            birthdate: new Date(),
+        },
+    })
+
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            await axios.post(`/api/person/create`, values);
+            // toast.success("Topic added !");
+            form.reset();
+
+            router.refresh();
+        } catch {
+            // toast.error("Something went wrong");
+            console.log("error")
+        }
+        console.log(values)
+    }
+
+    return (
+        <Form {...form} >
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 space-x-10">
+
+                <div className="flex gap-4 ">
+                    <FormField
+                        control={form.control}
+                        name="firstName"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>first name</FormLabel>
+                                <FormControl>
+                                    <Input type="text" placeholder="first name" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="lastName"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>last name</FormLabel>
+                                <FormControl>
+                                    <Input type="text" placeholder="last name" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+
+                <FormField
+                    control={form.control}
+                    name="sex"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Sex</FormLabel>
+                            <FormControl>
+                                <Input {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <FormField
+                    control={form.control}
+                    name="birthdate"
+                    render={({ field }) => (
+                        <FormItem className="flex flex-col">
+                            <FormLabel>Date of birth</FormLabel>
+                            <Popover>
+                                <PopoverTrigger asChild>
+                                    <FormControl>
+                                        <Button
+                                            variant={"outline"}
+                                            className={cn(
+                                                "w-[240px] pl-3 text-left font-normal",
+                                                !field.value && "text-muted-foreground"
+                                            )}
+                                        >
+                                            {field.value ? (
+                                                format(field.value, "PPP")
+                                            ) : (
+                                                <span>Pick a date</span>
+                                            )}
+                                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                        </Button>
+                                    </FormControl>
+                                </PopoverTrigger>
+                                <PopoverContent className="w-auto p-0" align="start">
+                                    <Calendar
+                                        mode="single"
+                                        selected={field.value}
+                                        onSelect={field.onChange}
+                                        disabled={(date) =>
+                                            date > new Date() || date < new Date("1900-01-01")
+                                        }
+                                        initialFocus
+                                    />
+                                </PopoverContent>
+                            </Popover>
+                            <FormDescription>
+                                Your date of birth is used to calculate your age.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    )
+}
