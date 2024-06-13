22390eea1f8637072501f327bb3ce486d58a50e7
Molina-Q <quentinmolina1@gmail.com>
2024-06-13 19:46:53 +0200

    feat: Add Toaster component to display notifications in the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -31,6 +32,7 @@ export default function RootLayout({
         <main className="p-6">
           {children}
         </main>
+        <Toaster />
       </body>
     </html>
   );
