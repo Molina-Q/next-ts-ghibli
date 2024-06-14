e9d680eacc85facfe517d8e807163834b78a77a4
Molina-Q <quentinmolina1@gmail.com>
2024-06-14 19:00:50 +0200

    feat: Create RoleForm component for creating a role

---
+++ app/roles/page.tsx
@@ -0,0 +1,84 @@
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { db } from "@/lib/db"
+import axios from "axios"
+import { useRouter } from "next/navigation"
+
+import { addDays, format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { Calendar } from "@/components/ui/calendar"
+import { cn } from "@/lib/utils"
+import { toast } from "sonner"
+
+const formSchema = z.object({
+    name: z.string().min(1, {
+        message: "name must not be empty.",
+    }),
+})
+
+export default function RoleForm() {
+    const router = useRouter();
+
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        },
+    })
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            await axios.post(`/api/role/create`, values);
+            toast.success("Genre created successfully !");
+            form.reset();
+
+            router.refresh();
+
+        } catch {
+            toast.error("Something went wrong");
+        }
+    }
+
+    return (
+        <Form {...form} >
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 space-x-10">
+
+                <h1 className='text-3xl'>Role's Form</h1>
+
+                <div className="flex gap-4 ">
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>role's name</FormLabel>
+                                <FormControl>
+                                    <Input type="text" placeholder="a role" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    )
+}
