b107346e45b97c1919d83d5da81a53a6a002da0b
Molina-Q <quentinmolina1@gmail.com>
2024-06-15 14:01:03 +0200

    refactor: show age of each person

--- app/persons/list/page.tsx
+++ app/persons/list/page.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { calculateAge } from '@/lib/calculateAge';
 import { db } from '@/lib/db';
 import { PrismaClient } from '@prisma/client';
 import React from 'react'
@@ -24,27 +25,25 @@ const ListPersons = async () => {
       <h1 className='text-3xl mx-auto'>List of Persons</h1>
 
       <div className='grid grid-cols-6 gap-4 p-12'>
-      
+
         {persons.map((person) => (
           <Card key={person.id} className='shadow-md hover:-translate-y-2 hover:shadow-xl transition-all'>
             <CardHeader>
               <CardTitle>{person.firstName} {person.lastName}</CardTitle>
-              <CardDescription>Born in {person.birthdate.getFullYear()}</CardDescription>
+              <CardDescription>Born in {person.birthdate.getFullYear()} | {calculateAge(person.birthdate)} years old </CardDescription>
             </CardHeader>
             <CardContent>
               <p>{person.directors[0] ? 'Director' : 'Jobless'}</p>
             </CardContent>
-            <CardFooter>
-              <p>
-                {
-                  (person.directors[0] && person.directors[0].movies) &&
-                  person.directors.map((director) => (
-                    director.movies.map((movie) => (
-                      movie.title
-                    ))
+            <CardFooter className='flex flex-col'>
+              {
+                (person.directors[0] && person.directors[0].movies) &&
+                person.directors.map((director) => (
+                  director.movies.map((movie) => (
+                    <p>{movie.title}</p>
                   ))
-                }
-              </p>
+                ))
+              }
             </CardFooter>
           </Card>
 
