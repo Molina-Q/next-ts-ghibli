aacd010cd3697e5d33243def7ed4c9abd19a3492
Molina-Q <quentinmolina1@gmail.com>
2024-06-18 23:43:49 +0200

    feat: Add actor and director fields to PersonForm

--- app/api/person/create/route.ts
+++ app/api/person/create/route.ts
@@ -9,6 +9,8 @@ export async function POST(
     try {
         const { lastName, firstName, sex, birthdate, director, actor } = await req.json();
 
+        const items = [];
+
         const person = await db.person.create({
             data: {
                 lastName,
@@ -18,7 +20,29 @@ export async function POST(
             }
         });
 
-        return NextResponse.json(person);
+        if (director == 1) {
+            const isDirector = await db.director.create({
+                data: {
+                    personId: person.id,
+                }
+            });
+
+            isDirector && items.push(isDirector);
+        }
+
+        if (actor == 1) {
+            const isActor = await db.actor.create({
+                data: {
+                    personId: person.id,
+                }
+            });
+
+            isActor && items.push(isActor);
+        }
+
+        person && items.push(person);
+
+        return NextResponse.json(items);
 
     } catch (error) {
         console.log("error create Movie : ", error);
