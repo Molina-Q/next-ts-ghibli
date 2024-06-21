6e2c1eb47fb5a4d1cf15f328ad49a6a41377cda4
Molina-Q <quentinmolina1@gmail.com>
2024-06-21 12:00:00 +0200

    added loading

---
+++ app/loading.tsx
@@ -0,0 +1,6 @@
+export default function Loading() {
+    // Or a custom loading skeleton component
+    return <div className="w-[100vw] h-[30vh] flex justify-center items-center">
+        <p className="text-2xl">Loading...</p>
+    </div> 
+}
