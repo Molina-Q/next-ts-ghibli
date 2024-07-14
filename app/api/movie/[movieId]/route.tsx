ad4770458f228237dc4052f739e4f2d04f8c342c
Molina-Q <quentinmolina1@gmail.com>
2024-07-14 21:56:26 +0200

    refactor: Extract movieId from URL query parameters in movie API route, doesnt work

--- app/api/movie/[movieId]/route.tsx
+++ app/api/movie/[movieId]/route.tsx
@@ -1,25 +1,25 @@
 import { db } from '@/lib/db';
-import { useParams } from 'next/navigation';
-import { NextResponse } from 'next/server';
-import React from 'react'
+import { NextRequest, NextResponse } from 'next/server';
 
-
-export async function GET(
-    req: Request
-) {
+export async function GET(params: { movieId: string }) {
     try {
-        const params = useParams();
+        // Extracting the movieId from the URL query parameters
+        const movieId = params.movieId;
+
+        if (!movieId) {
+            return new NextResponse("Movie ID is required", { status: 400 });
+        }
 
         const movie = await db.movie.findUnique({
             where: {
-                id: parseInt(params.movieId[0]),
+                id: parseInt(movieId),
             }
         });
 
         return NextResponse.json(movie);
 
     } catch (error) {
-        console.log("error create Movie : ", error);
+        console.log("error fetching Movie : ", error);
         return new NextResponse("Internal Server Error", { status: 500 });
     }
 }
